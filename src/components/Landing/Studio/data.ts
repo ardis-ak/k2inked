aa043fbcfdeac1bbede1eda1192d97184e35fad3
{Agata Kuklińska} <delicadency@gmail.com>
2025-12-21 17:38:24 +0100

    fix: update gendered language in studio and team descriptions

--- src/components/Landing/Studio/data.ts
+++ src/components/Landing/Studio/data.ts
@@ -1,6 +1,6 @@
 export const studioData = [
   "Jesteśmy studiem założonym przez dwie tatuatorki z wieloletnim doświadczeniem. Dogłębna wiedza o naszej branży sprawia, że łączymy profesjonalizm z klimatem, który przenosi Cię w nasz artystyczny świat.",
-  "Skład naszego studia tworzą artystki o różnych specjalizacjach i unikalnym stylu – dzięki temu każdy znajdzie tu coś dla siebie, od delikatnych, minimalistycznych wzorów po pełne detali, mocnej czerni, odważne kompozycje.",
+  "Skład naszego studia tworzą artyści o różnych specjalizacjach i unikalnym stylu – dzięki temu każdy znajdzie tu coś dla siebie, od delikatnych, minimalistycznych wzorów po pełne detali, mocnej czerni, odważne kompozycje.",
   "Wykonasz u nas rownież piercing, który podkreśli Twój charakter i dopełni wyjątkowy wizerunek.",
   "Przyjdź i zanurz się w świecie sztuki, a jedno z dzieł weź ze sobą, na zawsze, na skórze.",
 ] as const;
--- src/components/Landing/Team/data.ts
+++ src/components/Landing/Team/data.ts
@@ -1,6 +1,6 @@
 type TeamParagraphs = string[];
 
 export const teamParagraphs: TeamParagraphs = [
-  "Każda z nas ma swój unikalny styl i podejście do tatuażu, co pozwala nam sprostać różnorodnym oczekiwaniom naszych klientów. W naszym zespole znajdziesz artystki specjalizujące się w różnych technikach, od delikatnych linii po realistyczne portrety.",
+  "Każdy z nas ma swój unikalny styl i podejście do tatuażu, co pozwala nam sprostać różnorodnym oczekiwaniom naszych klientów. W naszym zespole znajdziesz artystów specjalizujących się w różnych technikach, od delikatnych linii po realistyczne portrety.",
   "Oferujemy nie tylko tatuaże, ale także profesjonalny piercing, który podkreśli Twój indywidualny styl. Nasze studio to miejsce, gdzie pasja do sztuki spotyka się z najwyższymi standardami higieny i bezpieczeństwa.",
 ];
